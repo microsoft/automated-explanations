80b98bbdac7240a62a94120731f2dfab11be3f01
Chandan Singh <chandan_singh@berkeley.edu>
2023-03-19 10:25:01 -0700

    update to 120 voxels

--- notebooks/03_fmri_process_results.py
+++ notebooks/03_fmri_process_results.py
@@ -85,10 +85,11 @@ if __name__ == '__main__':
     # results_dir = '/home/chansingh/mntv1/mprompt/feb12_fmri_sweep_gen_template1/'
     # results_dir = '/home/chansingh/mntv1/mprompt/mar7_test/'
     # results_dir = '/home/chansingh/mntv1/mprompt/mar8/'
-    results_dir = '/home/chansingh/mntv1/mprompt/mar9/'
+    # results_dir = '/home/chansingh/mntv1/mprompt/mar9/'
+    results_dir = '/home/chansingh/mntv1/mprompt/mar13/'
 
     r = imodelsx.process_results.get_results_df(results_dir, use_cached=False)
-    print(f'Loaded {r.shape[0]} results.')
+    print(f'Loaded {r.shape[0]} results')
     for num in [25, 50, 75, 100]:
         r[f'top_ngrams_module_{num}'] = r['explanation_init_ngrams'].apply(lambda x: x[:num])
         # r[f'top_ngrams_test_{num}'] = r.apply(lambda row: get_test_ngrams(voxel_num_best=row.module_num)[:num], axis=1)
