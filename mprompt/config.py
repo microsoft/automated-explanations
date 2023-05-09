9842df7d4c0d4c0822a670de8b1e5918ffee6043
Chandan Singh <chandan_singh@berkeley.edu>
2023-05-09 16:27:59 -0700

    update config paths

--- mprompt/config.py
+++ mprompt/config.py
@@ -1,6 +1,10 @@
-CACHE_DIR = '/accounts/campus/aliyahhsu/m_cache'
-# RESULTS_DIR = '/home/chansingh/mprompt/results/'
-RESULTS_DIR = '/accounts/campus/aliyahhsu/module-prompt/results'
-REPO_DIR = '/accounts/campus/aliyahhsu/module-prompt'
-# SAVE_DIR_FMRI = '/home/chansingh/mntv1/fmri/'
-SAVE_DIR_FMRI = '/home/chansingh/mntv1/deep-fMRI/opt_model/'
+import os.path
+if os.path.exists('/home/chansingh'):
+    RESULTS_DIR = '/home/chansingh/mprompt/results/'
+    SAVE_DIR_FMRI = '/home/chansingh/mntv1/deep-fMRI/opt_model/'
+    REPO_DIR = '/home/chansingh/mprompt/'
+    CACHE_DIR = '/home/chansingh/mntv1/mprompt/cache'
+else:
+    RESULTS_DIR = '/accounts/campus/aliyahhsu/module-prompt/results'
+    REPO_DIR = '/accounts/campus/aliyahhsu/module-prompt'
+    CACHE_DIR = '/accounts/campus/aliyahhsu/m_cache'
