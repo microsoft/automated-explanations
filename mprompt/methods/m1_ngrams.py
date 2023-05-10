7e7767e3c3434968a712c36892f9098c3c739e57
Chandan Singh <chandan_singh@berkeley.edu>
2023-05-10 14:54:55 -0700

    add results_synthetic.pkl

--- mprompt/methods/m1_ngrams.py
+++ mprompt/methods/m1_ngrams.py
@@ -10,6 +10,23 @@ from mprompt.config import CACHE_DIR
 import mprompt.data.data
 
 
+def _get_cache_filename(args, CACHE_DIR) -> str:
+    if args.module_name == "fmri":
+        return join(CACHE_DIR, "cache_ngrams", f"{args.module_name}_{args.subject}.pkl")
+    elif args.module_name == "old_fmri":
+        return join(CACHE_DIR, "cache_ngrams", f"{args.module_name}.pkl")
+    elif args.module_name == "dict_learn_factor":
+        return join(
+            CACHE_DIR,
+            "cache_ngrams",
+            f"{args.module_name}_{args.dl_task}_l{args.factor_layer}_i{args.factor_idx}.pkl",
+        )
+    else:
+        return join(
+            CACHE_DIR, "cache_ngrams", f"{args.module_name}_{args.module_num}.pkl"
+        )
+
+
 def explain_ngrams(
     args,
     X: List[str],
@@ -19,16 +36,12 @@ def explain_ngrams(
     num_top_ngrams: int = 75,
     use_cache: bool = True,
 ) -> List[str]:
-    """Note: this caches the call that gets the scores
-    """
+    """Note: this caches the call that gets the scores"""
     # get all ngrams
     tok = English(max_length=10e10)
-    X_str = ' '.join(X)
+    X_str = " ".join(X)
     ngrams_list = imodelsx.util.generate_ngrams_list(
-        X_str,
-        ngrams=ngrams,
-        tokenizer_ngrams=tok,
-        all_ngrams=all_ngrams
+        X_str, ngrams=ngrams, tokenizer_ngrams=tok, all_ngrams=all_ngrams
     )
 
     # get unique ngrams
@@ -37,34 +50,24 @@ def explain_ngrams(
 
     # compute scores and cache...
     # fmri should cache all preds together, since they are efficiently computed together
-    if args.module_name == 'fmri':
-        cache_file = join(CACHE_DIR, 'cache_ngrams',
-                            f'{args.module_name}_{args.subject}.pkl')
-    elif args.module_name == 'old_fmri':
-        cache_file = join(CACHE_DIR, 'cache_ngrams',
-                            f'{args.module_name}.pkl')
-    elif args.module_name == 'dict_learn_factor':
-        cache_file = join(CACHE_DIR, 'cache_ngrams',
-                            f'{args.module_name}_{args.dl_task}_l{args.factor_layer}_i{args.factor_idx}.pkl')
-    else:
-        cache_file = join(CACHE_DIR, 'cache_ngrams',
-                            f'{args.module_name}_{args.module_num}.pkl')
-    if use_cache and os.path.exists(cache_file):
-        ngram_scores = pkl.load(open(cache_file, 'rb'))
+    cache_filename = _get_cache_filename(args, CACHE_DIR)
+
+    if use_cache and os.path.exists(cache_filename):
+        ngram_scores = pkl.load(open(cache_filename, "rb"))
     else:
         call_parameters = inspect.signature(mod.__call__).parameters.keys()
-        print('predicting all ngrams...')
-        if args.module_name == 'dict_learn_factor':
+        print("predicting all ngrams...")
+        if args.module_name == "dict_learn_factor":
             ngram_scores = mod(ngrams_list, calc_ngram=True)
         else:
-            if 'return_all' in call_parameters:
+            if "return_all" in call_parameters:
                 ngram_scores = mod(ngrams_list, return_all=True)
             else:
                 ngram_scores = mod(ngrams_list)
-            
+
         if use_cache:
-            os.makedirs(dirname(cache_file), exist_ok=True)
-            pkl.dump(ngram_scores, open(cache_file, 'wb'))
+            os.makedirs(dirname(cache_filename), exist_ok=True)
+            pkl.dump(ngram_scores, open(cache_filename, "wb"))
 
     # multidimensional predictions
     if len(ngram_scores.shape) > 1 and ngram_scores.shape[1] > 1:
@@ -82,19 +85,24 @@ def explain_ngrams(
 
     # restrict top ngrams to alternative corpus
     if args.module_num_restrict >= 0:
-        print('before', ngrams_list)
+        print("before", ngrams_list)
         text_str_list_alt = mprompt.data.data.get_relevant_data(
-            args.module_name, args.module_num_restrict)
-        ngrams_set_alt = set(imodelsx.util.generate_ngrams_list(
-            ' '.join(text_str_list_alt),
-            ngrams=ngrams,
-            tokenizer_ngrams=tok,
-            all_ngrams=all_ngrams
-        ))
-        idxs_to_keep = np.array([i for i, ngram in enumerate(ngrams_list) if ngram in ngrams_set_alt])
+            args.module_name, args.module_num_restrict
+        )
+        ngrams_set_alt = set(
+            imodelsx.util.generate_ngrams_list(
+                " ".join(text_str_list_alt),
+                ngrams=ngrams,
+                tokenizer_ngrams=tok,
+                all_ngrams=all_ngrams,
+            )
+        )
+        idxs_to_keep = np.array(
+            [i for i, ngram in enumerate(ngrams_list) if ngram in ngrams_set_alt]
+        )
         ngrams_list = [ngrams_list[i] for i in idxs_to_keep]
         ngram_scores = ngram_scores[idxs_to_keep]
-        print('after', ngrams_list)
+        print("after", ngrams_list)
 
     # print(f'{ngram_scores=}')
     scores_top_idxs = np.argsort(ngram_scores)[::-1][:num_top_ngrams]
@@ -103,22 +111,22 @@ def explain_ngrams(
     return ngrams_top.flatten().tolist(), scores_top.flatten().tolist()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
     def mod(X):
         return np.arange(len(X)).astype(float)
 
     class a:
         noise_ngram_scores = 3
         seed = 100
-        module_name = 'emb_diff_d3'
+        module_name = "emb_diff_d3"
         module_num = 0
         module_num_restrict = -1
 
     explanation = explain_ngrams(
         a(),
-        ['and', 'i1', 'i2', 'i3', 'i4'],
+        ["and", "i1", "i2", "i3", "i4"],
         mod,
         use_cache=False,
-
     )
     print(explanation)
