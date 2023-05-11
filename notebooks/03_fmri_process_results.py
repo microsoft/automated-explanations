fix fmri normalization

notebooks/03_fmri_process_results.py
@@ -14,7 +14,9 @@ import sys
 import bert_score
 import numpy as np
 from typing import List
+
 tqdm.pandas()
+from copy import deepcopy
 from mprompt.modules.fmri_module import get_roi
 import mprompt.methods.evaluate as evaluate
 from mprompt.config import RESULTS_DIR
@@ -27,26 +29,30 @@ from mprompt.methods.evaluate import D5_Validator
 import torch.cuda
 from mprompt.config import CACHE_DIR
 
-def add_expl_preds_and_save(r, fname='results_fmri_full.pkl'):
-    """Calculate match between expl and test resp
-    """
-    dsets = joblib.load(join(SAVE_DIR_FMRI, 'stories', 'running_words.jbl'))
-    r['neg_dists_expl_test'] = [[] for _ in range(r.shape[0])]
-    r['rankcorr_expl_test'] = np.nan
 
-    mod = EmbDiffModule(task_str='')
+def add_expl_preds(r):
+    """Calculate match between expl and test resp"""
+    dsets = joblib.load(join(SAVE_DIR_FMRI, "stories", "running_words.jbl"))
+    r["neg_dists_expl_test"] = [[] for _ in range(r.shape[0])]
+    r["rankcorr_expl_test"] = np.nan
+
+    mod = EmbDiffModule(task_str="")
     for i in tqdm(range(r.shape[0])):
         row = r.iloc[i]
-        expl = row['top_explanation_init_strs']
-        subject = row['subject']
+        expl = row["top_explanation_init_strs"]
+        subject = row["subject"]
 
         # get resp
         dset = dsets[subject]
-        resp = dset['resp'][:, row['module_num']]
+        resp = dset["resp"][:, row["module_num"]]
 
         # check cache
-        cache_fname = join(CACHE_DIR, 'expl_preds', f'expl_test_{subject}_{expl.replace("/", "__")}.jbl')
-        os.makedirs(join(CACHE_DIR, 'expl_preds'), exist_ok=True)
+        cache_fname = join(
+            CACHE_DIR,
+            "expl_preds",
+            f'expl_test_{subject}_{expl.replace("/", "__")}.jbl',
+        )
+        os.makedirs(join(CACHE_DIR, "expl_preds"), exist_ok=True)
         loaded = False
         if os.path.exists(cache_fname):
             try:
@@ -54,13 +60,15 @@ def add_expl_preds_and_save(r, fname='results_fmri_full.pkl'):
                 loaded = True
             except:
                 pass
-            
+
         if not loaded:
             mod._init_task(task_str=expl)
-            strs_list = dset['words']
+            strs_list = dset["words"]
             neg_dists = [
                 mod(
-                    imodelsx.util.generate_ngrams_list(strs_list[j], ngrams=3, all_ngrams=True),
+                    imodelsx.util.generate_ngrams_list(
+                        strs_list[j], ngrams=3, all_ngrams=True
+                    ),
                     verbose=False,
                 ).mean()
                 for j in range(len(strs_list))
@@ -68,11 +76,11 @@ def add_expl_preds_and_save(r, fname='results_fmri_full.pkl'):
             joblib.dump(neg_dists, cache_fname)
 
         # neg dist closer to 0 should elicit higher response
-        r['neg_dists_expl_test'].iloc[i] = neg_dists
-        r['rankcorr_expl_test'].iloc[i] = scipy.stats.spearmanr(
-            neg_dists, resp, nan_policy='omit', alternative='greater').statistic
-        r.to_pickle(join(RESULTS_DIR, fname))
-
+        r["neg_dists_expl_test"].iloc[i] = neg_dists
+        r["rankcorr_expl_test"].iloc[i] = scipy.stats.spearmanr(
+            neg_dists, resp, nan_policy="omit", alternative="greater"
+        ).statistic
+    return r
 
     # mod = None
     # torch.cuda.empty_cache()
@@ -85,46 +93,81 @@ def add_expl_preds_and_save(r, fname='results_fmri_full.pkl'):
     #     for j in tqdm(range(len(strs_list)))
     # ]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # results_dir = '../results/feb12_fmri_sweep/'
     # results_dir = '/home/chansingh/mntv1/mprompt/feb12_fmri_sweep_gen_template1/'
     # results_dir = '/home/chansingh/mntv1/mprompt/mar7_test/'
     # results_dir = '/home/chansingh/mntv1/mprompt/mar8/'
     # results_dir = '/home/chansingh/mntv1/mprompt/mar9/'
-    results_dir = '/home/chansingh/mntv1/mprompt/mar13/'
+    results_dir = "/home/chansingh/mntv1/mprompt/mar13/"
 
     r = imodelsx.process_results.get_results_df(results_dir, use_cached=False)
-    print(f'Loaded {r.shape[0]} results')
+    print(f"Loaded {r.shape[0]} results")
     for num in [25, 50, 75, 100]:
-        r[f'top_ngrams_module_{num}'] = r['explanation_init_ngrams'].apply(lambda x: x[:num])
+        r[f"top_ngrams_module_{num}"] = r["explanation_init_ngrams"].apply(
+            lambda x: x[:num]
+        )
         # r[f'top_ngrams_test_{num}'] = r.apply(lambda row: get_test_ngrams(voxel_num_best=row.module_num)[:num], axis=1)
-        
-    print(f'Adding roi...')
-    r['roi_anat'] = r.progress_apply(lambda row: get_roi(voxel_num_best=row.module_num, roi_type='anat', subject=row.subject), axis=1)
-    r['roi_func'] = r.progress_apply(lambda row: get_roi(voxel_num_best=row.module_num, roi_type='func', subject=row.subject), axis=1)
+
+    print(f"Adding roi...")
+    r["roi_anat"] = r.progress_apply(
+        lambda row: get_roi(
+            voxel_num_best=row.module_num, roi_type="anat", subject=row.subject
+        ),
+        axis=1,
+    )
+    r["roi_func"] = r.progress_apply(
+        lambda row: get_roi(
+            voxel_num_best=row.module_num, roi_type="func", subject=row.subject
+        ),
+        axis=1,
+    )
 
     # Calculate train ngram correctness
-    print(f'Finding matching ngrams_module...')
+    print(f"Finding matching ngrams_module...")
     num_top_ngrams_expl = 75
-    correct_ngrams_module_scores, correct_ngrams_module_list = evaluate.calc_frac_correct_score(r, col_ngrams=f'top_ngrams_module_{num_top_ngrams_expl}')    
-    r['top_ngrams_module_correct'] = correct_ngrams_module_list
-    r['frac_top_ngrams_module_correct'] = r['top_ngrams_module_correct'].apply(lambda x: len(x) / num_top_ngrams_expl)
-    
+    (
+        correct_ngrams_module_scores,
+        correct_ngrams_module_list,
+    ) = evaluate.calc_frac_correct_score(
+        r, col_ngrams=f"top_ngrams_module_{num_top_ngrams_expl}"
+    )
+    r["top_ngrams_module_correct"] = correct_ngrams_module_list
+    r["frac_top_ngrams_module_correct"] = r["top_ngrams_module_correct"].apply(
+        lambda x: len(x) / num_top_ngrams_expl
+    )
+
     # Save results
-    r.to_pickle(join(RESULTS_DIR, 'results_fmri.pkl'))
+    r.to_pickle(join(RESULTS_DIR, "results_fmri.pkl"))
 
     # Add explanation<>test response match
     torch.cuda.empty_cache()
-    print('Saved original results, now computing expl<>resp match...')
-    add_expl_preds_and_save(r, fname='results_fmri_full.pkl')
+    print("Saved original results, now computing expl<>resp match...")
+    r = add_expl_preds(r)
+    r.to_pickle(join(RESULTS_DIR, "results_fmri_full.pkl"))
+
+    # Add score normalized by std over ngrams
+    scores_std = {}
+    for subject in ["UTS01", "UTS02", "UTS03"]:
+        ngram_scores_filename = join(CACHE_DIR, "cache_ngrams", f"fmri_{subject}.pkl")
+        ngram_scores = joblib.load(ngram_scores_filename)
+        ngram_scores_std = np.std(ngram_scores, axis=0)
+        scores_std[subject] = deepcopy(ngram_scores_std)
+
+    r["top_score_std"] = r.apply(
+        lambda x: scores_std[x["subject"]][x["module_num"]], axis=1
+    )
+    r["top_score_normalized"] = r["top_score_synthetic"] / r["top_score_std"]
+    r.to_pickle(join(RESULTS_DIR, "results_fmri_full.pkl"))
 
     # Unnecessary metrics
     # r['top_ngrams_test_correct_score'] = correct_ngrams_module_scores # these scores are basically just 0/1 for each ngram
     # r['expl_test_bert_score'] = r.progress_apply(lambda row: m4_evaluate.test_ngrams_bert_score(
-        # row['top_explanation_init_strs'], row['top_ngrams_test'].tolist()), axis=1)
+    # row['top_explanation_init_strs'], row['top_ngrams_test'].tolist()), axis=1)
 
     # Calculate test ngram correctness
     # num_top_ngrams_test = 75
-    # test_correct_score_list, correct_ngrams_test_list = m4_evaluate.calc_frac_correct_score(r, col_ngrams=f'top_ngrams_test_{num_top_ngrams_test}')    
+    # test_correct_score_list, correct_ngrams_test_list = m4_evaluate.calc_frac_correct_score(r, col_ngrams=f'top_ngrams_test_{num_top_ngrams_test}')
     # r['top_ngrams_test_correct'] = correct_ngrams_test_list
     # r['frac_top_ngrams_test_correct'] = r['top_ngrams_test_correct'].apply(lambda x: len(x) / num_top_ngrams_test)