dca679ee121dcf4d0e551c48202f17fa3765d657
Chandan Singh <chandan_singh@berkeley.edu>
2023-06-15 12:49:16 -0700

    temporal curve annotated plots

--- notebooks_stories/story_helper.py
+++ notebooks_stories/story_helper.py
@@ -1,9 +1,7 @@
 import numpy as np
 
-def get_resp_chunks(timing, paragraphs, resp_story, offset=8, apply_offset=True):
-    # return responses for each paragraph (after applying offset)
 
-    resp_chunks = []
+def get_start_end_times(timing, paragraphs):
     idx = 0
     start_times = []
     end_times = []
@@ -18,6 +16,14 @@ def get_resp_chunks(timing, paragraphs, resp_story, offset=8, apply_offset=True)
         end_times.append(timing["time_running"][idx - 1])
     start_times = (np.array(start_times) // 2).astype(int)
     end_times = (np.array(end_times) // 2).astype(int)
+    return start_times, end_times
+
+
+def get_resp_chunks(timing, paragraphs, resp_story, offset=8, apply_offset=True):
+    # return responses for each paragraph (after applying offset)
+
+    resp_chunks = []
+    start_times, end_times = get_start_end_times(timing, paragraphs)
 
     for i in range(len(start_times)):
         resp_paragraph = resp_story[:, start_times[i] : end_times[i]]
@@ -31,5 +37,5 @@ def get_resp_chunks(timing, paragraphs, resp_story, offset=8, apply_offset=True)
         # # # mid = resp_paragraph.shape[1] // 2
         # # resp_middle = resp_paragraph[:, mid - 1 : mid + 2]
         # mat[:, i] = resp_middle.mean(axis=1)
-        
+
     return resp_chunks
