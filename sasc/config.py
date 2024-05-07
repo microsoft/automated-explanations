e9b0b5aa802a96b0e749ae0cb7a1758e97d54fc2
Chandan Singh <chandan_singh@berkeley.edu>
2024-05-07 12:16:59 -0700

    update config

--- sasc/config.py
+++ sasc/config.py
@@ -1,13 +1,20 @@
 import os.path
+from os.path import join
 # if os.path.exists('/home/chansingh'):
-RESULTS_DIR = '/home/chansingh/automated-explanations/results/'
-STORIES_DIR = os.path.join(RESULTS_DIR, "stories")
-FMRI_DIR = '/home/chansingh/mntv1/deep-fMRI/'
-SAVE_DIR_FMRI = '/home/chansingh/mntv1/deep-fMRI/rj_models/'
 REPO_DIR = '/home/chansingh/automated-explanations/'
-CACHE_DIR = '/home/chansingh/mntv1/mprompt/cache'
+RESULTS_DIR = '/home/chansingh/automated-explanations/results/'
+FMRI_DIR = '/home/chansingh/mntv1/deep-fMRI/'
+
+STORIES_DIR = join(RESULTS_DIR, "stories")
+SAVE_DIR_FMRI = join(FMRI_DIR, 'sasc', 'rj_models')
+# '/home/chansingh/mntv1/mprompt/cache'
+CACHE_DIR = join(FMRI_DIR, 'sasc', 'mprompt', 'cache')
 # else:
 # RESULTS_DIR = '/accounts/campus/aliyahhsu/module-prompt/results'
 # SAVE_DIR_FMRI = '/var/tmp/ah_cache'
 # REPO_DIR = '/accounts/campus/aliyahhsu/module-prompt'
 # CACHE_DIR = '/var/tmp/ah_cache'
+
+
+# brain_tune
+PILOT_STORY_DATA_DIR = '/home/chansingh/mntv1/deep-fMRI/brain_tune/story_data'
