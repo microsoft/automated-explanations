56dd3935d30aa284e34ac0e14e1030aaa267b6f7
Chandan Singh <chandan_singh@berkeley.edu>
2024-11-10 11:09:43 -0800

    add RJ custom clusters + add pred corr

--- notebooks_stories/0_voxel_select/run_openai_calls.py
+++ notebooks_stories/0_voxel_select/run_openai_calls.py
@@ -4,22 +4,28 @@ import joblib
 
 subject = 'S02'
 suffix_setting = '_filt=0.15'
+# suffix_setting = '_rj_custom'
 
 
 d_selected = pd.read_pickle(
     f'top_clusters_by_pfc_cluster_{subject}{suffix_setting}.pkl')
 gpt4 = imodelsx.llm.get_llm('gpt-4')
 explanations = []
+print(d_selected.shape)
 prompt_template = '''Here is a list of phrases:
 {s}
 
-What is a common theme among these phrases? Return only a concise phrase.'''
+What is a common theme among these phrases (especially the top ones)? Return only a concise phrase.'''
 for i, row in d_selected.iterrows():
     top_ngrams = row['top_ngrams']
     s = '- ' + '\n- '.join(top_ngrams[:60])
     prompt = prompt_template.format(s=s)
     # print(prompt)
-    explanations.append(gpt4(prompt, use_cache=True))
+    explanation = gpt4(prompt, use_cache=True)
+    if explanation is None:
+        explanation = '<FAILED FOR CONTENT MODERATION>'
+        # explanation = gpt4(prompt, use_cache=False)
+    explanations.append(explanation)
     print(explanations)
 joblib.dump(
     explanations, f'explanations_by_pfc_cluster_{subject}{suffix_setting}.jbl')
