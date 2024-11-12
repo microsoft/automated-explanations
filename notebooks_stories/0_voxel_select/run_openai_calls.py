010f500749ac4f885608f5d557b77543a6603408
Chandan Singh <chandan_singh@berkeley.edu>
2024-11-12 14:43:47 -0800

    explain communication rois

--- notebooks_stories/0_voxel_select/run_openai_calls.py
+++ notebooks_stories/0_voxel_select/run_openai_calls.py
@@ -1,31 +1,51 @@
 import imodelsx.llm
 import pandas as pd
 import joblib
+import json
+
+
+prompt_template = '''Here is a list of phrases:
+{s}
+
+What is a common theme among these phrases (especially the top ones)? Return only a concise phrase.'''
 
 subject = 'S02'
 suffix_setting = '_filt=0.15'
 # suffix_setting = '_rj_custom'
 
 
-d_selected = pd.read_pickle(
-    f'top_clusters_by_pfc_cluster_{subject}{suffix_setting}.pkl')
-gpt4 = imodelsx.llm.get_llm('gpt-4')
-explanations = []
-print(d_selected.shape)
-prompt_template = '''Here is a list of phrases:
-{s}
+# d_selected = pd.read_pickle(
+# f'top_clusters_by_pfc_cluster_{subject}{suffix_setting}.pkl')
+# gpt4 = imodelsx.llm.get_llm('gpt-4')
+# explanations = []
+# print(d_selected.shape)
 
-What is a common theme among these phrases (especially the top ones)? Return only a concise phrase.'''
-for i, row in d_selected.iterrows():
-    top_ngrams = row['top_ngrams']
-    s = '- ' + '\n- '.join(top_ngrams[:60])
+# for i, row in d_selected.iterrows():
+#     top_ngrams = row['top_ngrams']
+#     s = '- ' + '\n- '.join(top_ngrams[:60])
+#     prompt = prompt_template.format(s=s)
+#     # print(prompt)
+#     explanation = gpt4(prompt, use_cache=True)
+#     if explanation is None:
+#         explanation = '<FAILED FOR CONTENT MODERATION>'
+#         # explanation = gpt4(prompt, use_cache=False)
+#     explanations.append(explanation)
+#     print(explanations)
+# joblib.dump(
+#     explanations, f'explanations_by_pfc_cluster_{subject}{suffix_setting}.jbl')
+
+
+explanations = {}
+top_ngrams_df = pd.read_pickle(
+    f'top_ngrams_custom_communication_{subject}.pkl')
+gpt4 = imodelsx.llm.get_llm('gpt-4')
+for k in top_ngrams_df.columns:
+
+    s = '- ' + '\n- '.join(top_ngrams_df[k].iloc[:100])
     prompt = prompt_template.format(s=s)
-    # print(prompt)
-    explanation = gpt4(prompt, use_cache=True)
-    if explanation is None:
-        explanation = '<FAILED FOR CONTENT MODERATION>'
-        # explanation = gpt4(prompt, use_cache=False)
-    explanations.append(explanation)
+    if not k in explanations:
+        # print(prompt)
+        explanations[k] = gpt4(prompt)
     print(explanations)
-joblib.dump(
-    explanations, f'explanations_by_pfc_cluster_{subject}{suffix_setting}.jbl')
+json.dump(explanations, open(
+    f'explanations_by_roi_communication_{subject}.json', 'w'), indent=4)
