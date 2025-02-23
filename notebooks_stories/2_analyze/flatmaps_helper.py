92cd24fd8d5db0bf61c554f30eb2f2e76f6e6197
Chandan Singh <chandan_singh@berkeley.edu>
2025-02-22 19:45:22 -0800

    update fed rois with 5th expl

--- notebooks_stories/2_analyze/flatmaps_helper.py
+++ notebooks_stories/2_analyze/flatmaps_helper.py
@@ -33,6 +33,7 @@ FED_DRIVING_EXPLANATIONS_S03 = {
     1: 'Positive Emotional Reactions',
     2: 'Body parts',
     3: 'Dialogue',
+    4: 'Negative Emotional Reactions',
 }
 
 FED_DRIVING_EXPLANATIONS_S02 = {
@@ -40,60 +41,8 @@ FED_DRIVING_EXPLANATIONS_S02 = {
     1: 'Introspection',
     2: 'Relationships',
     3: 'Sexual and Romantic Interactions',
+    4: 'Dialogue',
 }
-
-
-def load_custom_rois(subject, suffix_setting='_fedorenko'):
-    '''
-    Params
-    ------
-    subject: str
-        'S02' or 'S03'
-    suffix_setting: str
-        '' - load custom communication rois
-        '_fedorenko' - load fedorenko rois
-        '_spotlights' - load spotlights rois (there are a ton of these)
-    '''
-    if suffix_setting == '':
-        # rois_dict = joblib.load(join(regions_idxs_dir, f'rois_{subject}.jbl'))
-        # rois = joblib.load(join(FMRI_DIR, 'brain_tune/voxel_neighbors_and_pcs/', 'communication_rois_UTS02.jbl'))
-        rois = joblib.load(join(FMRI_DIR, 'brain_tune/voxel_neighbors_and_pcs/',
-                                f'communication_rois_v2_UT{subject}.jbl'))
-        rois_dict_raw = {i: rois[i] for i in range(len(rois))}
-        if subject == 'S02':
-            raw_idxs = [
-                [0, 7],
-                [3, 4],
-                [1, 5],
-                [2, 6],
-            ]
-        elif subject == 'S03':
-            raw_idxs = [
-                [0, 7],
-                [3, 4],
-                [2, 5],
-                [1, 6],
-            ]
-        return {
-            'comm' + str(i): np.vstack([rois_dict_raw[j] for j in idxs]).sum(axis=0)
-            for i, idxs in enumerate(raw_idxs)
-        }
-    elif suffix_setting == '_fedorenko':
-        if subject == 'S03':
-            rois_fedorenko = joblib.load(join(
-                FMRI_DIR, 'brain_tune/voxel_neighbors_and_pcs/', 'lang_localizer_UTS03.jbl'))
-        elif subject == 'S02':
-            rois_fedorenko = joblib.load(join(
-                FMRI_DIR, 'brain_tune/voxel_neighbors_and_pcs/', 'lang_localizer_UTS02_aligned.jbl'))
-        return {
-            'Lang-' + str(i): rois_fedorenko[i] for i in range(len(rois_fedorenko))
-        }
-        # rois_dict = rois_dict_raw
-    elif suffix_setting == '_spotlights':
-        rois_spotlights = joblib.load(join(
-            FMRI_DIR, 'brain_tune/voxel_neighbors_and_pcs/', f'all_spotlights_UT{subject}.jbl'))
-        return {'spot' + str(i): rois_spotlights[i][-1]
-                for i in range(len(rois_spotlights))}
 
 
 def load_known_rois(subject):
